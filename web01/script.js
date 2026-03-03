0bcd23b5e5276f8b3d3ec490812c36b1bf13d84d
worawut-kmu <worawut.kmu@gmail.com>
2026-03-03 11:51:36 +0700

    first commit

--- web01/script.js
+++ web01/script.js
@@ -1,1 +1,9 @@
 console.log('Hello My Website');
+
+// simple dark/light theme toggle
+const toggle = document.getElementById('toggleTheme');
+if (toggle) {
+    toggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+    });
+}
